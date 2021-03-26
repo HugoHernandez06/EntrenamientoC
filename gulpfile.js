3f41aacadc20e399fc01535112226b1454932b24
AndresParra01 <47034138+AndresParra01@users.noreply.github.com>
2021-03-26 06:37:39 -0500

    Commit final APK

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
-const{src, dest} = require('gulp');
+const {src,dest,series} = require('gulp');
 const sass = require('gulp-sass');
-const rename = require('gulp-rename');
+const rename = require("gulp-rename");
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
-
-
+const image = require('gulp-image');
+const destino = "../www";
 
 sass.compiler = require('dart-sass');
-
-
 
 function js() {
 return src(['./js/main.js'])
@@ -29,8 +27,36 @@ return src("./sass/**/*.scss")
 .pipe(dest("./css"));
 }
 
+function images(){
+	return src('./img/**/*')
+	.pipe(image())
+	.pipe(dest(`./${destino}/img`));
+}
 
+function mover_css(){
+	return src('./css/**/*')
+	.pipe(dest(`./${destino}/css`));
+}
+
+function mover_js(){
+	return src('./js/**/*')
+	.pipe(dest(`./${destino}/js`));
+}
+
+function mover_html(){
+	return src('./html/index.html')
+	.pipe(dest(`./${destino}`));
+}
+
+function mover_images(){
+	return src('./img/**/*')
+	.pipe(dest(`./${destino}/img`));
+}
 
 exports.css = css;
 exports.js = js;
+exports.images = images;
+exports.release = series(css,js,images,mover_css,mover_js,mover_html);
+exports.build = series(css,js,mover_css,mover_js,mover_html,mover_images);
+
 exports.default = ()=>{};
